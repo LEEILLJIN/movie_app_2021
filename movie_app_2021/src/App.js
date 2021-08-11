1a3d6bbcd39f0626740d40553b251ec1af13b7b4
Jin <qjsro368@gmail.com>
2021-08-11 14:52:59 +0900

    2021-08-11

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -5,22 +5,17 @@ import React from 'react';
 
 class App extends React.Component{
   state = {
-    count : 0
-  };
-  add = () => {
-    this.setState(current =>({count : current.count+1}));
-  };
-  minus = () => {
-    this.setState(current => ({count :  current.count-1}));
-  };//This(add minus) is javascript code, not react
-  // We don't write {this.add()} or {this.minus()} because we call the functions immediately
+    isLoading : true,
+    movies : []
+  }
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({ isLoading : false});
+    }, 6000);
+  }
   render(){
-    return(<div>
-     <h1>The number is {this.state.count}</h1>
-     <button onClick={this.add}>Add</button>
-     <button onClick={this.minus}>Minus</button>
-     </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
   }
 }
 
