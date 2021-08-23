570dc1bcbc11f99ca2193e277c9ae25fe0dcd64f
Jin <qjsro368@gmail.com>
2021-08-23 13:22:04 +0900

    08/23

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -2,20 +2,41 @@ import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
 import React from 'react';
+import axios from "axios";
+import Movie from './Movie';
 
 class App extends React.Component{
   state = {
     isLoading : true,
     movies : []
   }
+  getMovies = async () =>{
+    const {
+      data : {
+        data :{movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies , isLoading : false});
+  }
   componentDidMount(){
-    setTimeout(() =>{
-      this.setState({ isLoading : false});
-    }, 6000);
+   this.getMovies(); 
   }
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? "Loading..." 
+    : movies.map(movie => (//map함수는 반드시 무언가를 return해줘야한다. =>이면 return의 의미가 포함되어있는 것이다.
+        <Movie 
+          key ={movie.id}
+          id={movie.id} 
+          year={movie.year} 
+          title={movie.title} 
+          summary={movie.summary} 
+          poster={movie.medium_cover_image}
+       />
+      
+    
+    ))}
+    </div>
   }
 }
 
